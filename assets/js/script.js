d817d30e7a6098592a928700d07f5bbc6e89ea55
meshackodhiambo <meshackodhiambo019@gmail.com>
2024-10-22 14:53:59 +0300

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+const colorButton=document.getElementById('colorButton');
+
+colorButton.addEventListener('Click',()=>{
+    colorButton.style.backgroundColor=colorButton.style.backgroundColor === 'blue'? '4CAF58':'blue';
+    }
+
+
+);
